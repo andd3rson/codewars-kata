5f8c0e7ecb1e720f2c3044f93efdab92bb1b9de1
Anderson Conceição <andersonconceiicao@gmail.com>
2022-11-26 19:46:45 +1100

    feat: created first kata and started another one.

---
+++ take-an-array-adding-string/LikesFuncions.cs
@@ -0,0 +1,24 @@
+public static class LikeFunctions
+{
+    public static string LikeTransform(string[] names)
+    {
+        switch (names.Count())
+        {
+            case 0:
+                return "No one like this";
+            case 1:
+                return $"{names[0]} like this";
+
+            case 2:
+                return $"{names[0]} and {names[1]} like this";
+
+            case 3:
+                return $"{names[0]}, {names[1]} and {names[2]} like this";
+
+            default:
+                return $"{names[0]}, {names[1]} and {names.Count() - 2} Others like this";
+
+        }
+
+    }
+}
---
+++ take-an-array-adding-string/Program.cs
@@ -0,0 +1,22 @@
+﻿bool continued = false;
+List<string> names = new();
+Console.Write("Deseja adicionar y/n: ");
+var add = Console.ReadLine() == "y" ? true : false;;
+Console.Clear();
+if (add)
+{
+
+    do
+    {
+        Console.Write("add a new name: ");
+        names.Add(Console.ReadLine() ?? string.Empty);
+        Console.Clear();
+        Console.WriteLine("Would like to add anyone more?\n1 - Yes.\n0 - No. ");
+        continued = Console.ReadLine() == "0" ? false : true;
+        Console.Clear();
+    }
+    while (continued);
+
+}
+
+Console.WriteLine(LikeFunctions.LikeTransform(names));
